42bff2deec7267fd02be823b451c937ea27ed6d1
faq <127988214+FacundoMajda@users.noreply.github.com>
2024-09-13 14:36:42 -0300

    feat: implement CRUD operations for todos; add create, update, and delete functionalities

--- backend/src/controllers/todos.controllers.js
+++ backend/src/controllers/todos.controllers.js
@@ -4,8 +4,72 @@ export const getAllTodos = (req, res) => {
   if (!req.user) {
     return res.status(401).json({ message: "No autenticado" });
   }
-
   const todos = database.todos.filter((todo) => todo.owner === req.user.id);
-
   res.json({ todos });
+};
+
+export const createTodo = (req, res) => {
+  if (!req.user) {
+    return res.status(401).json({ message: "No autenticado" });
+  }
+
+  const { title } = req.body;
+
+  if (!title) {
+    return res.status(400).json({ message: "Falta el título" });
+  }
+
+  const newTodo = {
+    id: database.todos.length + 1,
+    title,
+    completed: false,
+    owner: req.user.id,
+  };
+
+  database.todos.push(newTodo);
+  return res.status(201).json({ todo: newTodo });
+};
+
+export const updateTodo = (req, res) => {
+  if (!req.user) {
+    return res.status(401).json({ message: "No autenticado" });
+  }
+
+  const { id } = req.params;
+  const { title, completed } = req.body;
+
+  const todo = database.todos.find(
+    (todo) => todo.id === parseInt(id) && todo.owner === req.user.id
+  );
+
+  if (!todo) {
+    return res.status(404).json({ message: "Todo no encontrado" });
+  }
+
+  todo.title = title !== undefined ? title : todo.title;
+  todo.completed = completed !== undefined ? completed : todo.completed;
+
+  return res.status(200).json({ message: "Todo actualizado" });
+};
+
+export const deleteTodo = (req, res) => {
+  if (!req.user) {
+    return res.status(401).json({ message: "No autenticado" });
+  }
+
+  const { id } = req.params;
+
+  const todo = database.todos.find(
+    (todo) => todo.id === parseInt(id) && todo.owner === req.user.id
+  );
+
+  if (!todo) {
+    return res.status(404).json({ message: "Todo no encontrado" });
+  }
+
+  database.todos = database.todos.filter(
+    (todo) => todo.id !== parseInt(id) || todo.owner !== req.user.id
+  );
+
+  return res.status(200).json({ message: "Todo eliminado" });
 };
--- backend/src/routes/todos.routes.js
+++ backend/src/routes/todos.routes.js
@@ -1,10 +1,18 @@
 import { Router } from "express";
-import { getAllTodos } from "../controllers/todos.controllers.js";
+import {
+  createTodo,
+  deleteTodo,
+  getAllTodos,
+  updateTodo,
+} from "../controllers/todos.controllers.js";
 import { authenticate } from "../middlewares/validar-jwt.js";
 
 const router = Router();
 
 router.get("/", authenticate, getAllTodos);
+router.post("/", authenticate, createTodo);
+router.put("/:id", authenticate, updateTodo);
+router.delete("/:id", authenticate, deleteTodo);
 
 const todoRouter = router;
 export default todoRouter;
